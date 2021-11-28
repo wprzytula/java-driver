1d372e7e1e603005acd0f4b4867bd37c23497b40
Karolina Drabik <kd417818@students.mimuw.edu.pl>
2021-11-28 18:59:44 +0100

    not compiling request handler

--- driver-examples/runOpenTelemetryExample.sh
+++ driver-examples/runOpenTelemetryExample.sh
@@ -1,1 +1,1 @@
-mvn -q exec:java -Dexec.mainClass="com.datastax.driver.examples.opentelemetry.ZipkinUsage"
+cd .. && mvn install -q -Dmaven.test.skip=true && cd driver-examples && mvn -q exec:java -Dexec.mainClass="com.datastax.driver.examples.opentelemetry.ZipkinUsage"
